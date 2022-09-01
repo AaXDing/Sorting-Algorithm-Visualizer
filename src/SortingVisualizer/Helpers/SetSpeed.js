4901c79404e3f9623690dad0861836a6e7908202
Yuheng Ding <yuhengding@s-128-97-168-169.resnet.ucla.edu>
2022-09-01 01:03:01 -0700

    animation change

--- src/SortingVisualizer/Helpers/SetSpeed.js
+++ src/SortingVisualizer/Helpers/SetSpeed.js
@@ -1,7 +1,7 @@
 export function setSpeed(size, speed){
     console.log(size);
     if(size === 30){
-        return (speed * 10);
+        return (speed * 20);
     }else if (size === 100){
         return speed;
     }else{
